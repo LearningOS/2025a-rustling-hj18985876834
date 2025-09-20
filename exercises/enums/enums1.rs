f76077a4ef5b204568ff145f332b2fda1b0904c5
HeJiang <2748432469@qq.com>
2025-09-20 19:49:37 +0800

    day1end

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,16 +2,20 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
+
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo("hello".into()));
+    println!("{:?}", Message::Move{x:10,y:10});
+    println!("{:?}", Message::ChangeColor(255,255,255));
 }
